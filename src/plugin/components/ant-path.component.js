acb6a90c0a16f0e30f119e1207ff0bbe0cfcc6a4
dolpsdw <dolpsdw@gmail.com>
2019-10-23 22:24:11 +0200

    Canvas Rework

--- src/plugin/components/ant-path.component.js
+++ src/plugin/components/ant-path.component.js
@@ -1,202 +1,194 @@
-import { FeatureGroup, polyline, canvas, Util } from "leaflet";
-
+import { FeatureGroup, polyline, canvas } from "leaflet";
+//* Closure @private uids to be used as key https://javascript.info/symbol
 const Layers = { main: Symbol("main"), pulse: Symbol("pulse") };
 
 /**
- * Builds the ant path polygon
+ * Builds the layers for each polyArray[{path: LatLng|MultiPolyline, polyOptions}]
+ * 1 layer foreach path, 1 layer for each diferent polyType in path (dash animations)
  * @class
  * @extends {L.FeatureGroup}
  */
 export default class AntPath extends FeatureGroup {
+  //this[Layers.main], this references are required for layer & polyline interfaces
   [Layers.main] = [];
-  [Layers.pulse] = null;
+  [Layers.pulse] = [];
 
   _map = null;
-  _path = null;
-  _animatedPathId = null;
-  _animatedPathClass = "leaflet-ant-path";
-  _reversePathClass = `${this._animatedPathClass}__reverse`;
-  _hardwareAccClass = "hardware-acceleration";
 
-  _defaultOptions = {
+  /**
+   * polyOptions to apply for each L.Path
+   * @typedef {Object<string,any>} polyOptions
+   * @property {polyline} use - L.pathType to use for construct the path
+   * @property {number} weight - weight of the path stroke
+   * @property {Renderer} renderer - render used to render polylayers
+   * @property {number} opacity - opacity of the stroke
+   * @property {string} color - hex color
+   */
+  /**
+   * default polyOption that will be merged on each L.Path
+   * @type {polyOptions}
+   * @memberof AntPath
+   */
+  _defaultPolyOptions = {
     use: polyline,
-    paused: false,
-    reverse: false,
-    hardwareAcceleration: false,
+    weight: 5,
+    renderer: canvas({ pane: "overlayPane" }),
+    opacity: 0.5,
+    color: "#0000FF"
+  };
+  /**
+   * dashedOptions to apply on the dashed animated path
+   * @typedef {Object<string,any>} dashedOptions
+   * @property {Renderer} renderer - render used to render dashedlayers
+   * @property {number} delay - speed of animation
+   * @property {string} dashArray - dashed pattern
+   * @property {string} color - hex color
+   * @property {boolean} paused - state of animation
+   * @property {boolean} reverse - direction of animation
+   */
+  /**
+   * default dashedOptions that will be merged with the input
+   * @type {dashedOptions}
+   * @memberof AntPath
+   */
+  _defaultdashedOptions = {
     renderer: canvas({ pane: "overlayPane" }),
     delay: 400,
-    dashArray: [10, 20],
-    weight: 5,
-    opacity: 0.5,
-    color: "#0000FF",
-    pulseColor: "#FFFFFF"
+    dashArray: "10,20",
+    color: "#FFFFFF",
+    paused: false,
+    reverse: false
+  };
+  /**
+   * polyDef to construct a Path
+   * @typedef {Object<string,any>} polyDef
+   * @property {latLngs|latlngs[]} path - single polyline latlngs or Multilatlngs
+   * @property {polyOptions} [polyOptions=_defaultOptions] - Path options to apply
+   */
+  /**
+   * polyArray
+   * @type {Array<polyDef>}
+   * @memberof AntPath
+   */
+  _polyArrayInput;
+  /**
+   * dashedOptions
+   * @type {dashedOptions}
+   * @memberof AntPath
+   */
+  _dashedOptionsInput = {};
+  /**
+   * Creates an instance of AntPath.
+   * @constructor
+   * @param {Array<polyDef>} polyArray - The Array of {@link polyDef} to be draw.
+   * @param {dashedOptions} [commondashedOptions=_defaultdashedOptions] - The {@link dashedOptions} of dashed animated path (Common to all the Paths)
+   * @memberof AntPath
+   */
+  constructor(polyArray, commondashedOptions) {
+    super();
+    //* this.options get feeded by Util.setOptions
+    this._polyArrayInput = polyArray.map(p => {
+      return { ...p, polyOptions: { ...this._defaultPolyOptions, ...p.polyOptions } };
+    });
+    this._dashedOptionsInput = { ...this._defaultdashedOptions, ...commondashedOptions };
+    // no need to call this twice this._mount();
+  }
+  _mount() {
+    //!One Layer per Color (~one per polyArray)(renderer by same canvas) + One layer per dashed polyType (all the dashed animated in same canvas together)
+    const dic = {};
+    this._polyArrayInput.forEach(i => {
+      const l = i.polyOptions.use(i.path, i.polyOptions);
+      this.addLayer(l);
+      this[Layers.main].push(l);
+      //* gather the diferent paths by L.path type
+      const ply = i.polyOptions.use.toString();
+      if (!dic[ply]) {
+        //if not initialized
+        dic[ply] = { path: [], use: i.polyOptions.use };
+      }
+      dic[ply].path.push(i.path); // Test if it do nice on combination of single and multi line
+    });
+    // Add the dashed layers
+    for (const key in dic) {
+      if (dic.hasOwnProperty(key)) {
+        const dl = dic[key].use(dic[key].path, this._dashedOptionsInput);
+        this.addLayer(dl);
+        this[Layers.pulse].push(dl);
+      }
+    }
+  }
+  /**
+   * Last Animation frame
+   * @type {number}
+   * @memberof AntPath
+   */
+  _frameRef;
+  //! since movecanvas is arrow could acces resume reverse? also maybe redraw only the this[Layers.pulse].foreach?
+  _moveCanvas = () => {
+    const dashDef = this._dashedOptionsInput.dashArray.split(",");
+    let totalDashDef = 0;
+    dashDef.forEach(n => totalDashDef + parseInt(n, 10));
+    // If at start of animation the dashedOffset = total DashDef, reset the animation (Or is mod = 0)
+    if (this._dashedOptionsInput.renderer._ctx.lineDashOffset % totalDashDef === 0) {
+      this._dashedOptionsInput.renderer._ctx.lineDashOffset = 0;
+    }
+    let speed = (1 / this._dashedOptionsInput.delay) * 1000;
+    if (this._dashedOptionsInput.reverse) {
+      speed = speed * -1;
+    }
+    this._dashedOptionsInput.renderer._ctx.lineDashOffset += speed; //not sure if non canvas renders have _ctx
+    this[Layers.pulse].forEach(l => l.redraw());
+    this._frameRef = window.requestAnimationFrame(this._moveCanvas);
   };
 
-  constructor(path, customOptions = {}) {
-    super();
-
-    Util.setOptions(this, { ...this._defaultOptions, ...customOptions });
-    this._path = path;
-    this._animatedPathId = `ant-path-${new Date().getTime()}`;
-    this._mount();
-  }
-
-  map(call) {
-    /**
-     * This prevents if some class extending AntPath to return a instance of AntPath itself instead of the invoker class
-     * Doing so, any child class will maintain the status of FUNCTOR object
-     */
-    const Species = this.constructor[Symbol.species];
-    return new Species(this._path.map(call), { ...this.options });
-  }
-
-  static get [Symbol.species]() {
-    return this;
-  }
-
-  get [Symbol.toStringTag]() {
-    return "L.Polyline.AntPath";
-  }
-
-  *[Symbol.iterator]() {
-    yield* this._path;
-  }
-
-  _processOptions() {
-    const {
-      options,
-      _animatedPathClass,
-      _reversePathClass,
-      _hardwareAccClass,
-      _animatedPathId
-    } = this;
-    const { reverse, hardwareAcceleration } = options;
-
-    let pathOpts = { ...options };
-    let pulseOpts = { ...options };
-
-    pulseOpts.color = pulseOpts.pulseColor || options.pulseColor;
-    pulseOpts.className = [
-      _animatedPathClass,
-      _animatedPathId,
-      reverse ? _reversePathClass : "",
-      hardwareAcceleration ? `${_animatedPathClass}__${_hardwareAccClass}` : ""
-    ].join(" ");
-
-    delete pathOpts.dashArray;
-
-    if (Array.isArray(pulseOpts.dashArray)) {
-      pulseOpts.dashArray = String(pulseOpts.dashArray);
-    }
-
-    return { pathOpts, pulseOpts };
-  }
-
-  _mount() {
-    const { pathOpts, pulseOpts } = this._processOptions();
-    const { use } = this.options;
-    //Todo make a foreach to allow multiple paths, and join them all in 1 pulse layer
-    this.addLayer((this[Layers.main] = use(this._path, pathOpts)));
-    this.addLayer((this[Layers.pulse] = use(this._path, pulseOpts)));
-  }
-
-  _calculateAnimationSpeed() {
-    const { options, _map, _animatedPathId } = this;
-
-    if (options.paused || !_map) {
-      return;
-    }
-
-    const zoomLevel = _map.getZoom();
-    const animatedPolyElements = document.getElementsByClassName(_animatedPathId);
-
-    //Get the animation duration (in seconds) based on the given delay and the current zoom level
-    const animationDuration = 1 + options.delay / 3 / zoomLevel + "s";
-
-    //TODO Use requestAnimationFrame to better support IE
-    const animationRules = ["-webkit-", "-moz-", "-ms-", "-o-", ""]
-      .map(prefix => `${prefix}animation-duration: ${animationDuration}`)
-      .join(";");
-
-    Array.from(animatedPolyElements, el => {
-      el.style.cssText = animationRules;
-      el.setAttribute("data-animated", "true");
-    });
-  }
-
-  _pureReverse() {
-    const el = this[Layers.pulse].getElement();
-    if (el) {
-      this.options.reverse
-        ? el.classList.remove(this._reversePathClass)
-        : el.classList.add(this._reversePathClass);
-    }
-  }
-
+  // AntPath leaflet events
   onAdd(map) {
     this._map = map;
-    this._map.on("zoomend", this._calculateAnimationSpeed, this);
+    //this._map.on("zoomend", this._calculateAnimationSpeed, this);
     this._mount();
-    this._calculateAnimationSpeed();
-    const moveCanvas = () => {
-      this._defaultOptions.renderer._ctx.lineDashOffset += (1 / this.options.delay) * 500;
-      this.redraw();
-      window.requestAnimationFrame(moveCanvas);
-    };
     //* requestAnimation have to be called ONCE for all the Canvas for performance...
-    window.requestAnimationFrame(moveCanvas);
+    this._frameRef = window.requestAnimationFrame(this._moveCanvas);
     return this;
   }
 
   onRemove(layer) {
     if (this._map) {
-      this._map.off("zoomend", this._calculateAnimationSpeed, this);
+      //this._map.off("zoomend", this._calculateAnimationSpeed, this);
       this._map = null;
     }
     if (layer) {
-      layer.removeLayer(this[Layers.main]).removeLayer(this[Layers.pulse]);
+      this[Layers.pulse].forEach(l => layer.removeLayer(l));
+      this[Layers.main].forEach(l => layer.removeLayer(l));
     }
     //* Maybe cancel requestAnimationFrame ? https://developer.mozilla.org/en-US/docs/Web/API/Window/cancelAnimationFrame
+    window.cancelAnimationFrame(this._frameRef);
     return this;
   }
-
+  // AntPath public Interface
   pause() {
-    const { paused } = this.options;
-
-    if (!paused) {
-      const el = this[Layers.pulse].getElement();
-      this.options.paused = true;
-
-      if (el) {
-        el.removeAttribute("style");
-        el.setAttribute("data-animated", "true");
-      }
-      return true;
+    if (!this._dashedOptionsInput.paused) {
+      this._dashedOptionsInput.paused = true;
+      window.cancelAnimationFrame(this._frameRef);
     }
-    return false;
   }
 
   resume() {
-    const { options } = this;
-    if (options.paused) {
-      options.paused = false;
-      this._calculateAnimationSpeed();
-      return true;
-    } else {
-      return false;
+    if (this._dashedOptionsInput.paused) {
+      this._dashedOptionsInput.paused = false;
+      this._frameRef = window.requestAnimationFrame(this._moveCanvas);
     }
   }
 
   //Feature Group methods overwriting
   bringToFront() {
-    this[Layers.main].bringToFront();
-    this[Layers.pulse].bringToFront();
+    this[Layers.main].forEach(l => l.bringToFront());
+    this[Layers.pulse].forEach(l => l.bringToFront());
     return this;
   }
 
   bringToBack() {
-    this[Layers.pulse].bringToBack();
-    this[Layers.main].bringToBack();
+    this[Layers.pulse].forEach(l => l.bringToBack());
+    this[Layers.main].forEach(l => l.bringToBack());
     return this;
   }
 
@@ -209,72 +201,32 @@ export default class AntPath extends FeatureGroup {
   }
 
   //Polyline interface
+  /**
+   * reStyle the common dash options
+   * @param {dashedOptions} options
+   * @memberof AntPath
+   */
   setStyle(options) {
-    const { paused, delay, reverse } = { ...this.options, ...options };
-    paused ? this.pause() : this.resume();
-
-    if (delay !== this.options.delay) {
-      this.options.delay = delay || this._defaultOptions.delay;
-      this._calculateAnimationSpeed();
-    }
-    if (typeof reverse !== "undefined" && reverse !== this.options.reverse) {
-      this._pureReverse();
-    }
-
-    this.options = { ...this.options, ...options };
-    const { pathOpts, pulseOpts } = this._processOptions();
-
-    this[Layers.main].setStyle(pathOpts);
-    this[Layers.pulse].setStyle(pulseOpts);
-
-    return this;
-  }
-
-  reverse() {
-    this._pureReverse();
-    this.options.reverse = !this.options.reverse;
+    // Todo compare new options to old and implement logic (at the end for redraw, inExample for color & dasharray => this[Layers.pulse].setStyle())
+    console.log(options);
     return this;
   }
 
   redraw() {
-    this[Layers.main].redraw();
-    this[Layers.pulse].redraw();
-
-    return this;
-  }
-
-  addLatLng(...args) {
-    this._path = [...this._path, args];
-    this[Layers.main].addLatLng(...args);
-    this[Layers.pulse].addLatLng(...args);
-
-    return this;
-  }
-
-  setLatLngs(...args) {
-    this._path = args;
-    this[Layers.main].setLatLngs(...args);
-    this[Layers.pulse].setLatLngs(...args);
-
+    this[Layers.main].forEach(l => l.redraw());
+    this[Layers.pulse].forEach(l => l.redraw());
     return this;
   }
 
   getLatLngs() {
-    return this[Layers.main].getLatLngs();
-  }
-
-  spliceLatLngs(...args) {
-    const latLngs = this[Layers.main].spliceLatLngs(...args);
-    this[Layers.pulse].spliceLatLngs(...args);
-
-    return latLngs;
+    return this[Layers.pulse].map(l => l.getLatLngs());
   }
 
   getBounds() {
-    return this[Layers.main].getBounds();
+    return this[Layers.pulse][0].getBounds();
   }
 
   toGeoJSON() {
-    return this[Layers.main].toGeoJSON();
+    return this[Layers.pulse][0].toGeoJSON();
   }
 }
